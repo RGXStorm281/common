91a2659d89ac0811a9a30871b840386239109577
RobinEpple <robin.epple01@gmail.com>
2025-02-21 18:12:11 +0100

    editorconfig initial try

---
+++ Common.Util.Test/CollectionExtensionsTest.cs
@@ -0,0 +1,23 @@
+namespace RobinEpple.Common.Util.Test;
+
+[TestClass]
+public class CollectionExtensionsTest
+{
+	private int Test { get; set; }
+
+	private List<int> List(params int[] items)
+		=> items.ToList();
+
+	private void AssertListEquals(List<int> left, List<int> right)
+	{
+		if (left.Count != right.Count)
+		{
+			Assert.Fail("");
+		}
+	}
+
+	public void WithPrepended_ShouldPrependItem()
+	{
+		var initial = List(1, 2, 3);
+	}
+}
