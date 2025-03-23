9ee6b0072f2132f9e8f5b184a299db322df5c921
RobinEpple <robin.epple01@gmail.com>
2025-03-23 18:38:23 +0100

    Startup script makes sure the container is initialized in a clean state

--- post-create.sh
+++ post-create.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-echo "Waiting for .NET installation..."
+
+# Remove cached files to make sure the container is initialized in a clean state
+echo "Cleaning repository"
+find /workspaces/common/src -type d \( -name "bin" -o -name "obj" \) -path "*" -exec rm -rf {} +
 
 # Wait until 'dotnet' command is available
+echo "Waiting for .NET installation..."
 until command -v dotnet &> /dev/null
 do
     sleep 1
 done
 
-echo "Dotnet is installed. Version:"
 # From here it is safe to call dotnet
+echo "Dotnet is installed. Version:"
 dotnet --version
 
+# Initialize dotnet
 echo "Initializing workloads..."
 sudo dotnet workload update
 
