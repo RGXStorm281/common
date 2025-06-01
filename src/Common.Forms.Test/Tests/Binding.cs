ae366b842545e2f6529cadcca0d693b225513435
RobinEpple <robin.epple01@gmail.com>
2025-06-01 16:35:20 +0200

    Instance Model binding tests

--- src/Common.Forms.Test/Tests/Binding.cs
+++ src/Common.Forms.Test/Tests/Binding.cs
@@ -156,45 +156,45 @@ public class Binding
 		Assert.AreEqual(sectionTarget, 5);
 	}
 
-	private BindingModel _model { get; set; } = new();
-
 	[TestMethod]
 	public void ExternalPropertyBinding_ShouldAccessPropertyFromBuildingContext()
 	{
+		var model = new BindingModel();
+
 		// initialize properties.
-		_model.BooleanProperty = false;
-		_model.DecimalProperty = 1;
-		_model.DoubleProperty = 1;
-		_model.FloatProperty = 1;
-		_model.LongProperty = 1;
-		_model.IntProperty = 1;
-		_model.TextProperty = "initial";
-		_model.TimestampProperty = new DateTime(2000, 1, 1);
-		_model.FileProperty = new FileValue("initial", [1, 2, 3]);
-		_model.CollectionProperty = [1, 2];
-		_model.SectionProperty = 3;
+		model.BooleanProperty = false;
+		model.DecimalProperty = 1;
+		model.DoubleProperty = 1;
+		model.FloatProperty = 1;
+		model.LongProperty = 1;
+		model.IntProperty = 1;
+		model.TextProperty = "initial";
+		model.TimestampProperty = new DateTime(2000, 1, 1);
+		model.FileProperty = new FileValue("initial", [1, 2, 3]);
+		model.CollectionProperty = [1, 2];
+		model.SectionProperty = 3;
 
 		// Build the form with all the bindings.
 		var form = new FormBuilder("Test")
-			.WithBooleanNode("Boolean", node => node.UsePropertyBinding(() => _model.BooleanProperty))
-			.WithNumberNode("Decimal", node => node.UsePropertyBinding(() => _model.DecimalProperty))
-			.WithNumberNode("Double", node => node.UsePropertyBinding(() => _model.DoubleProperty))
-			.WithNumberNode("Float", node => node.UsePropertyBinding(() => _model.FloatProperty))
-			.WithNumberNode("Long", node => node.UsePropertyBinding(() => _model.LongProperty))
-			.WithNumberNode("Int", node => node.UsePropertyBinding(() => _model.IntProperty))
-			.WithTextNode("Text", node => node.UsePropertyBinding(() => _model.TextProperty))
-			.WithTimestampNode("Timestamp", node => node.UsePropertyBinding(() => _model.TimestampProperty))
-			.WithFileNode("File", node => node.UsePropertyBinding(() => _model.FileProperty))
+			.WithBooleanNode("Boolean", node => node.UsePropertyBinding(() => model.BooleanProperty))
+			.WithNumberNode("Decimal", node => node.UsePropertyBinding(() => model.DecimalProperty))
+			.WithNumberNode("Double", node => node.UsePropertyBinding(() => model.DoubleProperty))
+			.WithNumberNode("Float", node => node.UsePropertyBinding(() => model.FloatProperty))
+			.WithNumberNode("Long", node => node.UsePropertyBinding(() => model.LongProperty))
+			.WithNumberNode("Int", node => node.UsePropertyBinding(() => model.IntProperty))
+			.WithTextNode("Text", node => node.UsePropertyBinding(() => model.TextProperty))
+			.WithTimestampNode("Timestamp", node => node.UsePropertyBinding(() => model.TimestampProperty))
+			.WithFileNode("File", node => node.UsePropertyBinding(() => model.FileProperty))
 			.WithCollectionNode(
 				"Collection",
 				(node, _) =>
-					node.UsePropertyBinding(() => _model.CollectionProperty)
+					node.UsePropertyBinding(() => model.CollectionProperty)
 						.UseTemplate("Template", template => template.UseInstanceModel(() => 4, out var _))
 			)
 			.WithTemplatedSection(
 				"Section",
 				(node, _) =>
-					node.UsePropertyBinding(() => _model.SectionProperty)
+					node.UsePropertyBinding(() => model.SectionProperty)
 						.UseTemplate("Template", template => template.UseInstanceModel(() => 5, out var _))
 			)
 			.Build();
@@ -247,29 +247,345 @@ public class Binding
 
 		form.WriteToBinding();
 
-		Assert.AreEqual(_model.BooleanProperty, true);
-		Assert.AreEqual(_model.DecimalProperty, 42);
-		Assert.AreEqual(_model.DoubleProperty, 42);
-		Assert.AreEqual(_model.FloatProperty, 42);
-		Assert.AreEqual(_model.LongProperty, 42);
-		Assert.AreEqual(_model.IntProperty, 42);
-		Assert.AreEqual(_model.TextProperty, "updated");
-		Assert.AreEqual(_model.TimestampProperty, new DateTime(2000, 1, 3));
-		Assert.AreEqual(_model.FileProperty.FileName, "updated");
-		Assert.AreEqual(_model.FileProperty.FileContents![0], 42);
-		Assert.AreEqual(_model.CollectionProperty.First(), 4);
-		Assert.AreEqual(_model.SectionProperty, 5);
+		Assert.AreEqual(model.BooleanProperty, true);
+		Assert.AreEqual(model.DecimalProperty, 42);
+		Assert.AreEqual(model.DoubleProperty, 42);
+		Assert.AreEqual(model.FloatProperty, 42);
+		Assert.AreEqual(model.LongProperty, 42);
+		Assert.AreEqual(model.IntProperty, 42);
+		Assert.AreEqual(model.TextProperty, "updated");
+		Assert.AreEqual(model.TimestampProperty, new DateTime(2000, 1, 3));
+		Assert.AreEqual(model.FileProperty.FileName, "updated");
+		Assert.AreEqual(model.FileProperty.FileContents![0], 42);
+		Assert.AreEqual(model.CollectionProperty.First(), 4);
+		Assert.AreEqual(model.SectionProperty, 5);
 	}
 
 	[TestMethod]
-	public void InstanceGetterSetterBinding_ShouldAccessValuesInInstanceModel() { }
+	public void InstanceGetterSetterBinding_ShouldAccessValuesInInstanceModel()
+	{
+		// Build the form with all the bindings.
+		var form = new FormBuilder("Test")
+			.UseInstanceModel(() => new BindingModel(), out var instanceBindingFactory)
+			.WithBooleanNode(
+				"Boolean",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.BooleanProperty,
+							(model, value) => model.BooleanProperty = value
+						)
+					)
+			)
+			.WithNumberNode(
+				"Decimal",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.DecimalProperty,
+							(model, value) => model.DecimalProperty = value
+						)
+					)
+			)
+			.WithNumberNode(
+				"Double",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.DoubleProperty,
+							(model, value) => model.DoubleProperty = value
+						)
+					)
+			)
+			.WithNumberNode(
+				"Float",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.FloatProperty,
+							(model, value) => model.FloatProperty = value
+						)
+					)
+			)
+			.WithNumberNode(
+				"Long",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.LongProperty,
+							(model, value) => model.LongProperty = value
+						)
+					)
+			)
+			.WithNumberNode(
+				"Int",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.IntProperty,
+							(model, value) => model.IntProperty = value
+						)
+					)
+			)
+			.WithTextNode(
+				"Text",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.TextProperty,
+							(model, value) => model.TextProperty = value
+						)
+					)
+			)
+			.WithTimestampNode(
+				"Timestamp",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.TimestampProperty,
+							(model, value) => model.TimestampProperty = value
+						)
+					)
+			)
+			.WithFileNode(
+				"File",
+				node =>
+					node.UseBinding(
+						instanceBindingFactory.CreateGetterSetterBinding(
+							model => model.TimestampProperty,
+							(model, value) => model.TimestampProperty = value
+						)
+					)
+			)
+			.WithCollectionNode(
+				"Collection",
+				(node, _) =>
+					node.UseBinding(
+							instanceBindingFactory.CreateGetterSetterBinding(
+								model => model.CollectionProperty,
+								(model, value) => model.CollectionProperty = value
+							)
+						)
+						.UseTemplate("Template", template => template.UseInstanceModel(() => 4, out var _))
+			)
+			.WithTemplatedSection(
+				"Section",
+				(node, _) =>
+					node.UseBinding(
+							instanceBindingFactory.CreateGetterSetterBinding(
+								model => model.SectionProperty,
+								(model, value) => model.SectionProperty = value
+							)
+						)
+						.UseTemplate("Template", template => template.UseInstanceModel(() => 5, out var _))
+			)
+			.Build();
+
+		// initialize properties.
+		var model = (BindingModel)form.Tags[IFormNodeBinding.InstanceModelTagName]!;
+		model.BooleanProperty = false;
+		model.DecimalProperty = 1;
+		model.DoubleProperty = 1;
+		model.FloatProperty = 1;
+		model.LongProperty = 1;
+		model.IntProperty = 1;
+		model.TextProperty = "initial";
+		model.TimestampProperty = new DateTime(2000, 1, 1);
+		model.FileProperty = new FileValue("initial", [1, 2, 3]);
+		model.CollectionProperty = [1, 2];
+		model.SectionProperty = 3;
+
+		var booleanNode = (IBooleanNode)form.Nodes.First(node => node.Name == "Boolean");
+		var decimalNode = (INumberNode)form.Nodes.First(node => node.Name == "Decimal");
+		var doubleNode = (INumberNode)form.Nodes.First(node => node.Name == "Double");
+		var floatNode = (INumberNode)form.Nodes.First(node => node.Name == "Float");
+		var longNode = (INumberNode)form.Nodes.First(node => node.Name == "Long");
+		var intNode = (INumberNode)form.Nodes.First(node => node.Name == "Int");
+		var textNode = (ITextNode)form.Nodes.First(node => node.Name == "Text");
+		var timestampNode = (ITimestampNode)form.Nodes.First(node => node.Name == "Timestamp");
+		var fileNode = (IFileNode)form.Nodes.First(node => node.Name == "File");
+		var collectionNode = (ICollectionNode)form.Nodes.First(node => node.Name == "Collection");
+		var sectionNode = (ITemplateNode)form.Nodes.First(node => node.Name == "Section");
+
+		// Load the state from the model.
+		form.LoadFromBinding();
+		Assert.AreEqual(booleanNode.Value, false);
+		Assert.AreEqual(decimalNode.Value, 1);
+		Assert.AreEqual(doubleNode.Value, 1);
+		Assert.AreEqual(floatNode.Value, 1);
+		Assert.AreEqual(longNode.Value, 1);
+		Assert.AreEqual(intNode.Value, 1);
+		Assert.AreEqual(textNode.Value, "initial");
+		Assert.AreEqual(timestampNode.Value, new DateTime(2000, 1, 1));
+		Assert.AreEqual(fileNode.Value.FileName, "initial");
+		Assert.AreEqual(fileNode.Value.FileContents![0], 1);
+		Assert.AreEqual(fileNode.Value.FileContents![1], 2);
+		Assert.AreEqual(fileNode.Value.FileContents![2], 3);
+		Assert.AreEqual(collectionNode.Instances.Count(), 2);
+		Assert.AreEqual(collectionNode.Instances.First().Tags[IFormNodeBinding.InstanceModelTagName], 1);
+		Assert.AreEqual(collectionNode.Instances.Skip(1).First().Tags[IFormNodeBinding.InstanceModelTagName], 2);
+		Assert.AreEqual(sectionNode.Instance!.Tags[IFormNodeBinding.InstanceModelTagName], 3);
+
+		// Alter the state in the form and write back.
+		booleanNode.Value = true;
+		decimalNode.Value = 42;
+		doubleNode.Value = 42;
+		floatNode.Value = 42;
+		longNode.Value = 42;
+		intNode.Value = 42;
+		textNode.Value = "updated";
+		timestampNode.Value = timestampNode.Value!.Value.AddDays(2);
+		fileNode.Value = new("updated", [42]);
+		collectionNode.Clear();
+		collectionNode.Instantiate(collectionNode.Templates.First());
+		sectionNode.Clear();
+		sectionNode.Instantiate(sectionNode.Templates.First());
+
+		form.WriteToBinding();
+
+		Assert.AreEqual(model.BooleanProperty, true);
+		Assert.AreEqual(model.DecimalProperty, 42);
+		Assert.AreEqual(model.DoubleProperty, 42);
+		Assert.AreEqual(model.FloatProperty, 42);
+		Assert.AreEqual(model.LongProperty, 42);
+		Assert.AreEqual(model.IntProperty, 42);
+		Assert.AreEqual(model.TextProperty, "updated");
+		Assert.AreEqual(model.TimestampProperty, new DateTime(2000, 1, 3));
+		Assert.AreEqual(model.FileProperty.FileName, "updated");
+		Assert.AreEqual(model.FileProperty.FileContents![0], 42);
+		Assert.AreEqual(model.CollectionProperty.First(), 4);
+		Assert.AreEqual(model.SectionProperty, 5);
+	}
 
 	[TestMethod]
-	public void InstanceGetterSetterBinding_ShouldFindTargetMultipleLayersAbove() { }
+	public void InstancePropertyBinding_ShouldAccessPropertyInInstanceModel()
+	{
+		// Build the form with all the bindings.
+		var form = new FormBuilder("Test")
+			.UseInstanceModel(() => new BindingModel(), out var instanceBindingFactory)
+			.WithBooleanNode(
+				"Boolean",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.BooleanProperty))
+			)
+			.WithNumberNode(
+				"Decimal",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.DecimalProperty))
+			)
+			.WithNumberNode(
+				"Double",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.DoubleProperty))
+			)
+			.WithNumberNode(
+				"Float",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.FloatProperty))
+			)
+			.WithNumberNode(
+				"Long",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.LongProperty))
+			)
+			.WithNumberNode(
+				"Int",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.IntProperty))
+			)
+			.WithTextNode(
+				"Text",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.TextProperty))
+			)
+			.WithTimestampNode(
+				"Timestamp",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.TimestampProperty))
+			)
+			.WithFileNode(
+				"File",
+				node => node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.TimestampProperty))
+			)
+			.WithCollectionNode(
+				"Collection",
+				(node, _) =>
+					node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.CollectionProperty))
+						.UseTemplate("Template", template => template.UseInstanceModel(() => 4, out var _))
+			)
+			.WithTemplatedSection(
+				"Section",
+				(node, _) =>
+					node.UseBinding(instanceBindingFactory.CreatePropertyBinding(model => model.SectionProperty))
+						.UseTemplate("Template", template => template.UseInstanceModel(() => 5, out var _))
+			)
+			.Build();
 
-	[TestMethod]
-	public void InstancePropertyBinding_ShouldAccessPropertyInInstanceModel() { }
+		// initialize properties.
+		var model = (BindingModel)form.Tags[IFormNodeBinding.InstanceModelTagName]!;
+		model.BooleanProperty = false;
+		model.DecimalProperty = 1;
+		model.DoubleProperty = 1;
+		model.FloatProperty = 1;
+		model.LongProperty = 1;
+		model.IntProperty = 1;
+		model.TextProperty = "initial";
+		model.TimestampProperty = new DateTime(2000, 1, 1);
+		model.FileProperty = new FileValue("initial", [1, 2, 3]);
+		model.CollectionProperty = [1, 2];
+		model.SectionProperty = 3;
 
-	[TestMethod]
-	public void InstancePropertyBinding_ShouldFindTargetMultipleLayersAbove() { }
+		var booleanNode = (IBooleanNode)form.Nodes.First(node => node.Name == "Boolean");
+		var decimalNode = (INumberNode)form.Nodes.First(node => node.Name == "Decimal");
+		var doubleNode = (INumberNode)form.Nodes.First(node => node.Name == "Double");
+		var floatNode = (INumberNode)form.Nodes.First(node => node.Name == "Float");
+		var longNode = (INumberNode)form.Nodes.First(node => node.Name == "Long");
+		var intNode = (INumberNode)form.Nodes.First(node => node.Name == "Int");
+		var textNode = (ITextNode)form.Nodes.First(node => node.Name == "Text");
+		var timestampNode = (ITimestampNode)form.Nodes.First(node => node.Name == "Timestamp");
+		var fileNode = (IFileNode)form.Nodes.First(node => node.Name == "File");
+		var collectionNode = (ICollectionNode)form.Nodes.First(node => node.Name == "Collection");
+		var sectionNode = (ITemplateNode)form.Nodes.First(node => node.Name == "Section");
+
+		// Load the state from the model.
+		form.LoadFromBinding();
+		Assert.AreEqual(booleanNode.Value, false);
+		Assert.AreEqual(decimalNode.Value, 1);
+		Assert.AreEqual(doubleNode.Value, 1);
+		Assert.AreEqual(floatNode.Value, 1);
+		Assert.AreEqual(longNode.Value, 1);
+		Assert.AreEqual(intNode.Value, 1);
+		Assert.AreEqual(textNode.Value, "initial");
+		Assert.AreEqual(timestampNode.Value, new DateTime(2000, 1, 1));
+		Assert.AreEqual(fileNode.Value.FileName, "initial");
+		Assert.AreEqual(fileNode.Value.FileContents![0], 1);
+		Assert.AreEqual(fileNode.Value.FileContents![1], 2);
+		Assert.AreEqual(fileNode.Value.FileContents![2], 3);
+		Assert.AreEqual(collectionNode.Instances.Count(), 2);
+		Assert.AreEqual(collectionNode.Instances.First().Tags[IFormNodeBinding.InstanceModelTagName], 1);
+		Assert.AreEqual(collectionNode.Instances.Skip(1).First().Tags[IFormNodeBinding.InstanceModelTagName], 2);
+		Assert.AreEqual(sectionNode.Instance!.Tags[IFormNodeBinding.InstanceModelTagName], 3);
+
+		// Alter the state in the form and write back.
+		booleanNode.Value = true;
+		decimalNode.Value = 42;
+		doubleNode.Value = 42;
+		floatNode.Value = 42;
+		longNode.Value = 42;
+		intNode.Value = 42;
+		textNode.Value = "updated";
+		timestampNode.Value = timestampNode.Value!.Value.AddDays(2);
+		fileNode.Value = new("updated", [42]);
+		collectionNode.Clear();
+		collectionNode.Instantiate(collectionNode.Templates.First());
+		sectionNode.Clear();
+		sectionNode.Instantiate(sectionNode.Templates.First());
+
+		form.WriteToBinding();
+
+		Assert.AreEqual(model.BooleanProperty, true);
+		Assert.AreEqual(model.DecimalProperty, 42);
+		Assert.AreEqual(model.DoubleProperty, 42);
+		Assert.AreEqual(model.FloatProperty, 42);
+		Assert.AreEqual(model.LongProperty, 42);
+		Assert.AreEqual(model.IntProperty, 42);
+		Assert.AreEqual(model.TextProperty, "updated");
+		Assert.AreEqual(model.TimestampProperty, new DateTime(2000, 1, 3));
+		Assert.AreEqual(model.FileProperty.FileName, "updated");
+		Assert.AreEqual(model.FileProperty.FileContents![0], 42);
+		Assert.AreEqual(model.CollectionProperty.First(), 4);
+		Assert.AreEqual(model.SectionProperty, 5);
+	}
 }
