3e9fb4a6185cec0e1df2a86c1334382f6c7ebd59
RobinEpple <robin.epple01@gmail.com>
2025-07-25 11:22:34 +0200

    Error Pages overhaul

--- src/Common.WebUi.Test/Code/Controllers/HomeController.cs
+++ src/Common.WebUi.Test/Code/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Net;
 using Common.WebUi.Test.Models;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Common.WebUi.Test.Controllers;
@@ -19,8 +21,31 @@ public class HomeController : Controller
 	}
 
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-	public IActionResult Error()
+	public IActionResult Error(int? statusCode)
 	{
-		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		// Determine the request-id.
+		var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+		// The exception handler feature can be null, if the method is called for a status code page, e.g. a "true" 404 and not one triggered by an exception.
+		var exceptionHandler = HttpContext.Features.Get<IExceptionHandlerFeature>();
+		var error = exceptionHandler?.Error;
+
+		// Choose the page to display according to the status code.
+		string view = statusCode switch
+		{
+			(int)HttpStatusCode.NotFound => "NotFound",
+			(int)HttpStatusCode.Forbidden => "Forbidden",
+			(int)HttpStatusCode.Conflict => "Conflict",
+			_ => "Error",
+		};
+
+		// Build the view model.
+		var viewModel = new ErrorViewModel(requestId, error);
+		if (viewModel.ShowRequestId)
+		{
+			_logger.LogError($"Showing error for request #{requestId}: {viewModel.Error?.Message}");
+		}
+
+		return View(view, viewModel);
 	}
 }
--- src/Common.WebUi.Test/Code/Models/ErrorViewModel.cs
+++ src/Common.WebUi.Test/Code/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
 namespace Common.WebUi.Test.Models;
 
-public class ErrorViewModel
+public class ErrorViewModel(string? requestId, Exception? error)
 {
-	public string? RequestId { get; set; }
+	public string? RequestId { get; set; } = requestId;
+	public Exception? Error { get; } = error;
 
 	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
